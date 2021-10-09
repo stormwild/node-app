4b221681dae8b261175ce865a85b59f7a7987611
Alexander R Torrijos <artorrijos@gmail.com>
2021-10-09 22:16:21 +0800

    EJS working

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
 
+const { PORT } = process.env;
+
 const app = express();
 
 app.use(morgan("tiny")); // combined
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.set("views", "./src/views");
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello App");
+  res.render("index", {
+    title: "Welcome to Globomantics",
+    data: ["a", "b", "c"],
+  });
 });
 
-app.listen(3000, () => {
-  debug(`Listening on port ${chalk.green("3000")}`);
+app.listen(PORT, () => {
+  debug(`Listening on port ${chalk.green(PORT)}`);
 });
